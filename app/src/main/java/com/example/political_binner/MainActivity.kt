532d801aff0ca6121737ae2c949492e008e8d5fc
M.arslan <arslanapas@gmail.com>
2022-09-25 05:51:04 -0700

    this is new app

---
+++ app/src/main/java/com/example/political_binner/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.political_binner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
